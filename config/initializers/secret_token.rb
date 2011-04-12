3e6fa0f22dd0214a10cce23f23465accfd71c139
Manish <manish.das@sprout-technology.com>
2011-04-12 11:01:17 +0545

    Initialized the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Writeboard::Application.config.secret_token = 'f324b9647412142531948151f2c607a8444182972b0aae50ccdeca2cecbb6a7dff40bd8d5312f998ca96d554044cefa3a0097ad3f6be7ae8bc45be08b170f7a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Writeboard::Application.config.session_store :cookie_store, :key => '_writeboard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Writeboard::Application.config.session_store :active_record_store
